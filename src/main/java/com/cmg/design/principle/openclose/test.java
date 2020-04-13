3586bc7b05196db1777c21738a8a8b9d6b5e4cde
qin.wang <qin.wang@kingstarfintech.com>
2020-04-13 20:41:17 +0800

    demo

--- src/main/java/com/cmg/design/principle/openclose/test.java
+++ src/main/java/com/cmg/design/principle/openclose/test.java
@@ -3,6 +3,6 @@ package com.cmg.design.principle.openclose;
 public class test {
     public static void main(String[] args) {
         ICourse javaCourse = new JavaCourse(123,"java课程",1235);
-        System.out.println(javaCourse.toString());
+        System.out.println("课程ID："+javaCourse.getId()+",课程名称："+javaCourse.getName()+",课程价格："+javaCourse.getPrice());
     }
 }
